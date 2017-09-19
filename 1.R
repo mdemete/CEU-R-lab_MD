2c52fd114eebeb100d42502ba38d815b4e840139
mdemete <demeter.meli.91@gmail.com>
2017-09-19 14:20:08 +0200

    Learned basic operators

---
+++ 1.R
@@ -0,0 +1,36 @@
+1+3
+3*2
+3^2
+
+pi
+pi *2
+'pi'
+letters
+LETTERS
+
+
+str(letters)
+letters[1]
+month.abb #abbreviated month names
+month.name
+
+?pi
+apropos('month') #lists all R functions / constants including the subject
+
+x = 4 #bad code style
+x <- 4 #good
+x / 2
+
+## TODO square root of x
+sqrt(x)
+x^0.5
+
+##define functions
+f <- function(x) {
+  2 * x + 1
+  }
+f(x)  
+x <- c(1, 2, 3, 4, 5)
+f(x)
+class(x)
+str(x)
